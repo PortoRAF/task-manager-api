b1ac5ed95310cfe7c45d3a066bdbc2ed32006ee0
Renato Porto <renatoafporto@gmail.com>
2020-03-29 16:38:45 -0300

    Initial commit

---
+++ mongodb.js
@@ -0,0 +1,51 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connect to database");
+    }
+
+    const db = client.db(databaseName);
+
+    db.collection("users").insertOne(
+      {
+        name: "Renato",
+        age: 37
+      },
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert user");
+        }
+
+        console.log(result.ops);
+      }
+    );
+
+    db.collection("users").insertMany(
+      [
+        {
+          name: "Jen",
+          age: 28
+        },
+        {
+          name: "Gunther",
+          age: 27
+        }
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert documents");
+        }
+
+        console.log(result.ops);
+      }
+    );
+  }
+);
