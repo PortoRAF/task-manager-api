8a11c78e01e830540e0174ca2ba6b241ec6bb261
Renato Porto <renatoafporto@gmail.com>
2020-03-30 14:27:49 -0300

    Deleting entries on mongodb

--- mongodb.js
+++ mongodb.js
@@ -11,38 +11,10 @@ MongoClient.connect(connectionURL, clientOptions, (error, client) => {
 
   const db = client.db(databaseName);
 
-  // db.collection("users")
-  //   .updateOne(
-  //     {
-  //       _id: new ObjectID("5e80eb236a8f8d3b04582c6a")
-  //     },
-  //     {
-  //       $inc: {
-  //         age: -5
-  //       }
-  //     }
-  //   )
-  //   .then(result => {
-  //     console.log(result);
-  //   })
-  //   .catch(error => {
-  //     console.log(error);
-  //   });
-
   db.collection("tasks")
-    .updateMany(
-      { completed: false },
-      {
-        $set: {
-          completed: true
-        }
-      }
-    )
-    .then(result => {
-      const { matchedCount, modifiedCount } = result;
-      console.log(
-        `Successfully matched ${matchedCount} and modified ${modifiedCount} items.`
-      );
+    .deleteOne({
+      description: "Scratch the itch"
     })
+    .then(result => console.log("Deleted count:", result.deletedCount))
     .catch(err => console.log(err));
 });
