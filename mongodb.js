aec0bd67e8b80526402babe2cd98e7c61e64ecf9
Renato Porto <renatoafporto@gmail.com>
2020-03-30 12:50:52 -0300

    Updating documents in mongodb

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,4 @@
 const { MongoClient, ObjectID } = require("mongodb");
-// const MongoClient = mongodb.MongoClient;
-// const ObjectID = mongodb.ObjectID
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
@@ -13,37 +11,38 @@ MongoClient.connect(connectionURL, clientOptions, (error, client) => {
 
   const db = client.db(databaseName);
 
-  // db.collection("users").findOne(
-  //   { _id: new ObjectID("5e80ecedc76c5f1d489e4c4d") },
-  //   (error, user) => {
-  //     if (error) {
-  //       return console.log("Unable to fetch");
-  //     }
-
-  //     if (!user) {
-  //       return console.log("No entries found");
-  //     }
-
-  //     console.log(user);
-  //   }
-  // );
-
   // db.collection("users")
-  //   .find({ name: "Renato" })
-  //   .toArray((error, users) => {
-  //     console.log(users);
+  //   .updateOne(
+  //     {
+  //       _id: new ObjectID("5e80eb236a8f8d3b04582c6a")
+  //     },
+  //     {
+  //       $inc: {
+  //         age: -5
+  //       }
+  //     }
+  //   )
+  //   .then(result => {
+  //     console.log(result);
+  //   })
+  //   .catch(error => {
+  //     console.log(error);
   //   });
 
-  db.collection("tasks").findOne(
-    { _id: new ObjectID("5e80edd5c5712b1ee88ce379") },
-    (error, task) => {
-      console.log(task);
-    }
-  );
-
   db.collection("tasks")
-    .find({ completed: false })
-    .toArray((error, tasks) => {
-      console.log(tasks);
-    });
+    .updateMany(
+      { completed: false },
+      {
+        $set: {
+          completed: true
+        }
+      }
+    )
+    .then(result => {
+      const { matchedCount, modifiedCount } = result;
+      console.log(
+        `Successfully matched ${matchedCount} and modified ${modifiedCount} items.`
+      );
+    })
+    .catch(err => console.log(err));
 });
