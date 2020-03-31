ae7a259ebc17b61473e34aa300b25a207b260905
Renato Porto <renatoafporto@gmail.com>
2020-03-31 16:55:07 -0300

    User login authentication

--- src/routers/user.js
+++ src/routers/user.js
@@ -5,10 +5,22 @@ const router = new express.Router();
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
   try {
-    await user.save();
+    await user.save({ j: true });
     res.status(201).send(user);
   } catch (error) {
     res.status(400).send(error);
+  }
+});
+
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    res.send(user);
+  } catch (error) {
+    res.status(400).send();
   }
 });
 
