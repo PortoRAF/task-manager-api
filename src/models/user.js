280d81ac6a127370aa49fa99a4b067dd5529371a
Renato Porto <renatoafporto@gmail.com>
2020-04-01 02:00:54 -0300

    Updated users routers with added authentication

--- src/models/user.js
+++ src/models/user.js
@@ -51,6 +51,17 @@ const userSchema = new mongoose.Schema({
   ]
 });
 
+userSchema.methods.toJSON = function () {
+  const user = this;
+  // const userObject = user.toObject();
+  // delete userObject.password;
+  // delete userObject.tokens;
+  // return userObject;
+
+  const { name, email, age } = user.toObject();
+  return { name, email, age };
+};
+
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
   const token = jwt.sign(
