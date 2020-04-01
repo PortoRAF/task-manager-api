62bd2938e96a286d9f0054aa146dcd047e516707
Renato Porto <renatoafporto@gmail.com>
2020-03-31 22:57:40 -0300

    Insert user auth tokens

--- src/routers/user.js
+++ src/routers/user.js
@@ -5,8 +5,9 @@ const router = new express.Router();
 router.post('/users', async (req, res) => {
   const user = new User(req.body);
   try {
-    await user.save({ j: true });
-    res.status(201).send(user);
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (error) {
     res.status(400).send(error);
   }
@@ -18,7 +19,8 @@ router.post('/users/login', async (req, res) => {
       req.body.email,
       req.body.password
     );
-    res.send(user);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
   } catch (error) {
     res.status(400).send();
   }
